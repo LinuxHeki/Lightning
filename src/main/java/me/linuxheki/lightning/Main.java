d34218f94c39f102e1f9bb54f87af36dc23ca08a
LinuxHeki <unconfigured@null.spigotmc.org>
2021-08-27 08:27:24 +0200

    Edit files

--- src/main/java/me/linuxheki/lightning/Main.java
+++ src/main/java/me/linuxheki/lightning/Main.java
@@ -1,5 +1,6 @@
 package me.linuxheki.lightning;
 
+import me.linuxheki.lightning.events.Lightning;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -7,7 +8,7 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(new Lightning(), this);
     }
 
     @Override
